4c5a1b1c6919984fe4277b5b4d4e9dbb1de91e42
Singh, Surendra <sbonam@sapient.com>
2016-02-10 10:37:11 -0800

    Initial commit

---
+++ SwiftDropDownList/Annotation.swift
@@ -0,0 +1,35 @@
+//
+//  Annotation.swift
+//  DropDownList
+//
+//  Created by Singh, Surendra on 2/10/16.
+//  Copyright © 2016 Singh, Surendra. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Annotation : UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override func drawRect(rect: CGRect) {
+        
+        let ctx : CGContextRef = UIGraphicsGetCurrentContext()!
+        
+        CGContextBeginPath(ctx)
+        CGContextMoveToPoint(ctx, CGRectGetMinX(rect), CGRectGetMaxY(rect))
+        CGContextAddLineToPoint(ctx, CGRectGetMaxX(rect), CGRectGetMaxY(rect))
+        CGContextAddLineToPoint(ctx, (CGRectGetMaxX(rect)/2.0), CGRectGetMinY(rect))
+        CGContextClosePath(ctx)
+        
+        CGContextSetRGBFillColor(ctx, 0.0, 0.0, 0.0,1.0);
+        CGContextFillPath(ctx);
+    }
+} 
---
+++ SwiftDropDownList/SwiftDropDownList.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDropDownList.h
+//  SwiftDropDownList
+//
+//  Created by Singh, Surendra on 2/10/16.
+//  Copyright © 2016 Singh, Surendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftDropDownList.
+FOUNDATION_EXPORT double SwiftDropDownListVersionNumber;
+
+//! Project version string for SwiftDropDownList.
+FOUNDATION_EXPORT const unsigned char SwiftDropDownListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDropDownList/PublicHeader.h>
+
+
